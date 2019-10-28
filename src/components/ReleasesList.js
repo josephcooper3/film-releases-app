c2d2c8063dda22e3eb99a78b52a56d83f5ef9390
Joe Cooper <josephcooper3@gmail.com>
2019-10-28 17:18:05 +0000

    adds releaseNodes

--- src/components/ReleasesList.js
+++ src/components/ReleasesList.js
@@ -4,10 +4,15 @@ import Release from "./Release";
 class ReleasesList extends Component {
 
   render() {
+
+    const releaseNodes = this.props.data.map(release => {
+      return <Release name={release.name} url={release.url} key={release.id}></Release>
+    })
+
     return (
       <div className="releases-list">
         <h2>Releases List</h2>
-        <Release/>
+        {releaseNodes}
       </div>
     )
   }
--- src/containers/ReleasesBox.js
+++ src/containers/ReleasesBox.js
@@ -3,11 +3,44 @@ import ReleasesList from "../components/ReleasesList";
 
 class ReleasesBox extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: "Western Stars",
+          url: "https://www.imdb.com/title/tt10687158/?ref_=rlm"
+        },
+        {
+          id: 2,
+          name: "Doctor Sleep",
+          url: "https://www.imdb.com/title/tt5606664/?ref_=rlm"
+        },
+        {
+          id: 3,
+          name: "Sorry We Missed You",
+          url: "https://www.imdb.com/title/tt8359816/?ref_=rlm"
+        },
+        {
+          id: 4,
+          name: "After The Wedding", 
+          url: "https://www.imdb.com/title/tt7985692/?ref_=rlm"
+        },
+        {
+          id: 5,
+          name: "Brittany Runs a Marathon",
+          url: "https://www.imdb.com/title/tt7671064/?ref_=rlm"
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="releases-box">
         <h1>Releases Box</h1>
-        <ReleasesList/>
+        <ReleasesList data={this.state.data}/>
       </div>
     )
   }
