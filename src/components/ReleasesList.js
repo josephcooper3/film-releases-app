81e40eef9cd2bf5cab2ba77147e679263d7a3d50
Joe Cooper <josephcooper3@gmail.com>
2019-10-28 16:28:32 +0000

    adds app structure

---
+++ src/components/ReleasesList.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+import Release from "./Release";
+
+
+export default ReleasesList;
---
+++ src/containers/ReleasesBox.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+import ReleasesList from "../components/ReleasesList"
+
+
+export default ReleasesBox;
